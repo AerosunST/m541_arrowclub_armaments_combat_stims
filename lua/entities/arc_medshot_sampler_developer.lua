8f431b6618f87f62995d1e19dc31b24cab1e5f63
Midawek <midawek@pm.me>
2026-09-13 13:33:56 +0200

    made a typo lmfao

--- lua/entities/arc_medshot_sampler_developer.lua
+++ lua/entities/arc_medshot_sampler_developer.lua
@@ -3,7 +3,7 @@ AddCSLuaFile()
 ENT.PrintName = "Combat Stim Variety Pack (x9999)"
 ENT.Spawnable = true
 ENT.Category = "Arrowclub Armaments Combat Stims"
-ENT.SubCategory = "StimPack"
+ENT.SubCategory = "StimPacks"
 ENT.Type = "anim"
 ENT.Base = "arc_medshot_base"
 ENT.AdminOnly = true
